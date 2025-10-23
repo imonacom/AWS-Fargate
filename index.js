53f7703547015b4f43b73031f16453a808df0c76
Erdiaslan <erdias333ea@gmail.com>
2025-10-23 19:18:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ connection.connect(err => {
 
 // Basit endpoint
 app.get('/', (req, res) => {
-  res.send('🚀 Fargate CI/CD test — new version deployed automaticallddddy!');
+  res.send('🚀 Fargate CI/CD test — new version deployed automatically!');
 });
 
 // Users endpoint
